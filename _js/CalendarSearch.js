8041d5816b60ac6ca0705c6a72144e88b5578c19
Redvus <a.suvorof@gmail.com>
2025-02-05 15:59:55 +0400

    Попытка сделать фильтр в имеющемся календаре.

--- _js/CalendarSearch.js
+++ _js/CalendarSearch.js
@@ -5,19 +5,28 @@ class CalendarSearch {
     }
 
     initLayout() {
-        this.calendarContentTitle = document.querySelector('.calendar-content__title');
-        this.calResult = this.calendarContentTitle.textContent.includes('Библиотека №5');
-        for (let i = 0; i < this.calendarContentTitle.length; i++) {
-            if (this.calResult) {
-                console.log(this.calResult);
-            }
-        }
-        // this.calendarContentTitle.forEach((el) => {
-        //     if (el === this.calendarContentTitle.textContent.includes('Библиотека №5')) {
-        //
+        this.calendarContent = document.querySelectorAll('.calendar-content');
+        this.mainContentCalendarDate = document.querySelectorAll('.main-content__calendar-date');
+        this.calendarContentEvent = document.querySelectorAll('.calendar-content__event');
+        // this.calendarContentTitle = document.querySelector('.calendar-content__title');
+        // this.calResult = this.calendarContent.textContent.includes('Библиотека №5');
+        // for (let i = 0; i < this.calendarContent.length; i++) {
+        //     if (this.calResult) {
+        //         console.log(this.calResult);
         //     }
-        // });
-        // this.calResult = ;
+        // }
 
+        this.calendarContent.forEach((el) => {
+            this.calendarContentEvent.forEach(elem => {
+                if (!el.textContent.includes('№5')) {
+                    // for (let i = 0; i < this.mainContentCalendarDate.length; i++) {
+                    //     console.log(this.mainContentCalendarDate[i]);
+                    //     // this.mainContentCalendarDate[i].style.opacity = '0';
+                    // }
+                    // console.log(el.id);
+                    elem.style.display = 'none';
+                }
+            });
+        });
     }
 }
